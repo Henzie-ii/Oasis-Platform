edbd380112a0abfce2e78be109c6d3a98dc8da21
Henzie <henzie0203@gmail.com>
2026-04-11 18:45:06 +0200

    feat(auth/models): add CustomUser class

--- backend/apps/authentication/models.py
+++ backend/apps/authentication/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import AbstractUser 
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    #we use AbstractUser to extend the default user model(password, email, username, etc) and add custom fields if needed
+    email = models.EmailField(unique=True)
+    username = models.CharField(min_length=3,max_length=20, uique=True)
+    bio  = models.TextField(blank=True, null=True)
+    profile_pic = models.ImageField(upload_to="profile_pics", blank=True, null=True)
+
+    EMAIL_FIELD = "email"
+    SURNAME_FIELD = "username"
+
+    def __str__(self):
+        return self.username
+
